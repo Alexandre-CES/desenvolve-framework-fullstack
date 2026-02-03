16231577925aef5fd1cc3229d6b7ca68751002e8
Alexandre <alexandreces666@gmail.com>
2026-02-03 12:09:48 -0300

    static files

--- meu_projeto/meu_app/admin.py
+++ meu_projeto/meu_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Produto, Categoria
 
-# Register your models here.
+class ProdutpAdmin(admin.ModelAdmin):
+  list_display = ('nome', 'preco', 'descricao')
+  search_fields = ('nome', 'descricao')
+  list_filter = ('preco',)
+  list_editable = ('preco',)
+
+admin.site.register(Produto, ProdutpAdmin)
+admin.site.register(Categoria)
---
+++ meu_projeto/static/js/script.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', function(){
+  alert('Alerta de javascript');
+})
