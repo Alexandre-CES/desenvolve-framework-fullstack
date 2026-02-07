1596e726f396f7127ad918cf226087b73db28a6a
Alexandre <alexandreces666@gmail.com>
2026-02-06 22:08:30 -0300

    loja

--- projetoMysql/projeto_mysql/projeto_mysql/settings.py
+++ projetoMysql/projeto_mysql/projeto_mysql/settings.py
@@ -72,13 +72,25 @@ WSGI_APPLICATION = 'projeto_mysql.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+'''
 
+DATABASES = {
+  'default':{
+    'ENGINE':'django.db.backends.mysql',
+    'NAME':'meu_projeto_db',
+    'USER':'django_user',
+    'PASSWORD':'minhasenha123',
+    'HOST':'localhost',
+    'PORT':'3306',
+  }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
