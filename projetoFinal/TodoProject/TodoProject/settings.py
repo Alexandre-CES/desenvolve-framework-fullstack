61046d3a94aee18c1d10a89912b8ddd2f29f6e76
Alexandre <alexandreces666@gmail.com>
2026-03-04 15:14:19 -0300

    feat: Task

--- projetoFinal/TodoProject/TodoProject/settings.py
+++ projetoFinal/TodoProject/TodoProject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ToDoApp.apps.TodoappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
