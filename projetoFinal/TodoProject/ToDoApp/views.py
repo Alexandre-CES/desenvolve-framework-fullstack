fcda504d1fb134b662c7231e451f6b67a391c7d9
Alexandre <alexandreces666@gmail.com>
2026-03-10 16:33:28 -0300

    feat: tasks

--- projetoFinal/TodoProject/ToDoApp/views.py
+++ projetoFinal/TodoProject/ToDoApp/views.py
@@ -22,4 +22,32 @@ def index(request):
 @login_required
 def task_list(request):
   tasks = Task.objects.filter(user=request.user)
-  return render(request, 'task_list.html')
+  return render(request, 'task_list.html', {'tasks':tasks})
+
+@login_required
+def task_create(request):
+  form = TaskForm(request.POST or None)
+  if form.is_valid():
+    task = form.save(commit=False)
+    task.user = request.user
+    task.save()
+    return redirect('task_list')
+  return render(request, 'task_form.html', {'form': form})
+
+@login_required
+def task_update(request, pk):
+  task = Task.objects.get(pk=pk)
+  if task.user != request.user:
+    return redirect('task_list')
+  form = TaskForm(request.POST or None, instance=task)
+  if form.is_valid():
+    form.save()
+    return redirect('task_list')
+  return render(request, 'task_form.html', {'form':form})
+
+@login_required
+def task_delete(request, pk):
+  task = Task.objects.get(pk=pk)
+  if task.user == request.user:
+    task.delete()
+  return redirect('task_list')
